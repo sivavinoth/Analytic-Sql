4d1621a9b2e78887a73e24bdb1f2b0ac02c81dbb
siva vinoth <88775535+sivavinoth@users.noreply.github.com>
2023-03-16 22:26:17 +0530

    Add files via upload

---
+++ SQl1/create.sql
@@ -0,0 +1,8 @@
+CREATE TABLE ml_job(
+    skill VARCHAR(200),
+    project INT,
+    connection INT,
+    next_topic VARCHAR(100)
+
+
+);
---
+++ SQl1/insert.sql
@@ -0,0 +1,4 @@
+INSERT into ml_job(skill ,connection,project,next_topic) VALUES ("python",0,3,"sklearn");
+INSERT into ml_job (skill ,connection,project,next_topic)VALUES ("mogodb",2,6,"tensorflow");
+INSERT into ml_job (skill ,connection,project,next_topic)VALUES ("sql",4,8,"plotty");
+INSERT into ml_job(skill ,connection,project,next_topic) VALUES ("deeplearning",8,10,"nlp");
