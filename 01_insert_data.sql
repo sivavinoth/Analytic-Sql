e296273453a93e1d7c338c5d3b0d56ca20fdbc6d
sivavinothjm <mjsivavinoth@gmail.com>
2023-03-31 18:50:33 +0530

    added values to the tables

---
+++ 01_insert_data.sql
@@ -0,0 +1,20 @@
+-- INSERT INTO users(names,salary,current_status) VALUES('surya',180000,'self-employed');
+-- INSERT INTO users (names,salary,current_status)VALUES('siva vinoth',190000,'self-employed');
+-- INSERT INTO users(names,salary,current_status) VALUES('richard',150000,'employed');
+-- INSERT INTO users(names,salary,current_status)VALUES('riyas',110000,'unemployed');
+-- INSERT INTO users (names,salary,current_status)VALUES('micheal',130000,'unemployed');
+
+INSERT INTO users ( names, salary, current_status)
+VALUES
+    ('michael', 130000, 'unemployed'),
+    ('jane', 95000, 'employed'),
+    ('peter',  75000, 'employed'),
+    ('susan',  85000, 'unemployed'),
+    ('john',  120000, 'employed'),
+    ('emily',  65000, 'unemployed'),
+    ('david', 110000, 'employed'),
+    ('kate',  90000, 'employed'),
+    ('robert',  80000, 'unemployed'),
+    ('olivia',  100000, 'employed');
+
+
---
+++ 03_insert_data.sql
@@ -0,0 +1,14 @@
+INSERT INTO conversations
+(user_name, employer_name,messages) 
+values
+ ('vinoth','google llc','we hired you'),
+ ('Rajesh', 'Microsoft Corporation', 'we hired you'),
+('Samantha', 'Amazon.com, Inc.', 'we selected you'),
+('Tom', 'Apple Inc.', 'you got the job'),
+('Priya', 'Facebook, Inc.', 'we are excited to have you join us'),
+('Michael', 'Twitter, Inc.', 'we would like to extend an offer to you'),
+('Lisa', 'Tesla, Inc.', 'we are pleased to offer you a position'),
+('David', 'Uber Technologies, Inc.', 'we want you on our team'),
+('Karen', 'Netflix, Inc.', 'we are thrilled to welcome you aboard'),
+('Surya', 'Intel Corporation', 'we have decided to hire you'),
+('Richard', 'IBM Corporation', 'we are looking forward to having you join our company');
